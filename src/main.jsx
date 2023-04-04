f9076a8b67e238e6ac8ff462d762e24dad5d33a3
mahedi6666655555 <mahedinir34678@gmail.com>
2023-04-04 20:58:33 +0600

    router set up is done

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,55 @@
-import React from 'react'
+import React, { Children } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Shop from './components/Shop/Shop';
+import Home from './layouy/Home';
+import Order from './components/Order';
+import Login from './components/Login';
+import Inventory from './components/Inventory';
+
+let router=createBrowserRouter(
+  [
+    {
+      path:"/",
+      element:<Home></Home>,
+     children:[
+      {
+        path:'Shop',  
+        element:<Shop></Shop>
+
+      },
+      
+      {
+        path:'order',  
+        element:<Order></Order>
+
+      },
+      {
+        path:'Login',  
+        element:<Login></Login>
+
+      },
+      {
+        path:'Inventory',  
+        element:<Inventory></Inventory>
+
+      },
+
+     ]
+    }
+  ]
+)
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 )
